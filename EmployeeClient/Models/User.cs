b5817784aea89f7d3f1d30fdf667127509ea36c2
Christian Sejer <christian_sejer@hotmail.com>
2020-11-17 16:14:11 +0100

    Added employee registration

---
+++ EmployeeClient/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EmployeeClient.Models
+{
+    public class User
+    {
+        [JsonPropertyName("userID")]
+        public int UserID { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string Contact { get; set; }
+
+        [JsonPropertyName("securityLevel")]
+        public int SecurityLevel { get; set; }
+
+        [JsonPropertyName("position")]
+        public string Position { get; set; }
+
+        [JsonPropertyName("storeID")]
+        public int StoreID { get; set; }
+    }
+}
