2847e44e674cf349c91d47ed99c36609265d4ba9
Christian Sejer <christian_sejer@hotmail.com>
2020-11-30 14:37:05 +0100

    Some image stuff :/

---
+++ EmployeeClient/Data/ImageService/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace EmployeeClient.Data.ImageService
+{
+    interface IImageService
+    {
+        Task UploadImage(Image image, int id);
+    }
+}
---
+++ EmployeeClient/Data/ImageService/ImageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace EmployeeClient.Data.ImageService
+{
+    public class ImageService : IImageService
+    {
+        HttpClient client = new HttpClient();
+
+        public async Task UploadImage(Image image, int id)
+        {
+            throw new NotImplementedException();
+            
+        }
+    }
+}
