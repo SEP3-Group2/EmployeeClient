4679a8569bda45894dd863d62a4cc8eeea76cf00
Kevin Pongó <293123@VIAUC.DK>
2020-12-08 11:18:13 +0100

    asd2.0

---
+++ EmployeeClient/Data/TransactionService/ITransactionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeClient.Data
+{
+    interface ITransactionService
+    {
+        Task UpdateToReady(int transactionid);
+        Task UpdateToDelivered(int transactionid);
+    }
+}
